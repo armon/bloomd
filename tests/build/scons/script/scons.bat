e0aabb8c0ea83a051969426a5df11bd625fa8ab5
Armon Dadgar <armon.dadgar@gmail.com>
2012-01-29 21:19:49 -0800

    Added unit test

---
+++ tests/build/scons/script/scons.bat
@@ -0,0 +1,34 @@
+@REM Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011 The SCons Foundation
+@REM src/script/scons.bat 5357 2011/09/09 21:31:03 bdeegan
+@echo off
+set SCONS_ERRORLEVEL=
+if "%OS%" == "Windows_NT" goto WinNT
+
+@REM for 9x/Me you better not have more than 9 args
+python -c "from os.path import join; import sys; sys.path = [ join(sys.prefix, 'Lib', 'site-packages', 'scons-2.1.0'), join(sys.prefix, 'Lib', 'site-packages', 'scons'), join(sys.prefix, 'scons-2.1.0'), join(sys.prefix, 'scons')] + sys.path; import SCons.Script; SCons.Script.main()" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@REM no way to set exit status of this script for 9x/Me
+goto endscons
+
+@REM Credit where credit is due:  we return the exit code despite our
+@REM use of setlocal+endlocal using a technique from Bear's Journal:
+@REM http://code-bear.com/bearlog/2007/06/01/getting-the-exit-code-from-a-batch-file-that-is-run-from-a-python-program/
+
+:WinNT
+setlocal
+@REM ensure the script will be executed with the Python it was installed for
+set path=%~dp0;%~dp0..;%path%
+@REM try the script named as the .bat file in current dir, then in Scripts subdir
+set scriptname=%~dp0%~n0.py
+if not exist %scriptname% set scriptname=%~dp0Scripts\%~n0.py
+python "%scriptname%" %*
+endlocal & set SCONS_ERRORLEVEL=%ERRORLEVEL%
+
+if NOT "%COMSPEC%" == "%SystemRoot%\system32\cmd.exe" goto returncode
+if errorlevel 9009 echo you do not have python in your PATH
+goto endscons
+
+:returncode
+exit /B %SCONS_ERRORLEVEL%
+
+:endscons
+call :returncode %SCONS_ERRORLEVEL%
---
+++ tests/test_bitmap.c
@@ -0,0 +1,282 @@
+#include <check.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include "../src/bitmap.h"
+
+/*
+cbloom_bitmap *bitmapFromFile(int fileno, size_t len) {
+*/
+
+START_TEST(make_anonymous_bitmap)
+{
+    // Use -1 for anonymous
+    cbloom_bitmap *map = bitmapFromFile(-1, 4096);
+    fail_unless(map != NULL);
+}
+END_TEST
+
+START_TEST(make_bitmap_zero_size)
+{
+    cbloom_bitmap *map = bitmapFromFile(-1, 0);
+    fail_unless(map == NULL);
+}
+END_TEST
+
+START_TEST(make_bitmap_bad_fileno)
+{
+    cbloom_bitmap *map = bitmapFromFile(500, 4096);
+    fail_unless(map == NULL);
+}
+END_TEST
+
+/*
+cbloom_bitmap *bitmapFromFilename(char* filename, size_t len, int create, int resize) {
+*/
+
+START_TEST(make_bitmap_nofile)
+{
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/asdf123", 4096, 0, 0);
+    fail_unless(map == NULL);
+}
+END_TEST
+
+START_TEST(make_bitmap_nofile_create)
+{
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/mmap_nofile_create", 4096, 1, 1);
+    unlink("/tmp/mmap_nofile_create");
+    fail_unless(map != NULL);
+}
+END_TEST
+
+START_TEST(make_bitmap_resize)
+{
+    int fh = open("/tmp/mmap_resize", O_RDWR|O_CREAT);
+    fchmod(fh, 0777);
+    fail_unless(fh > 0);
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/mmap_resize", 4096, 0, 1);
+    unlink("/tmp/mmap_resize");
+    fail_unless(map != NULL);
+}
+END_TEST
+
+/*
+ *size_t bitmapBitsize(cbloom_bitmap *map);
+ */
+
+START_TEST(bitmap_size_anonymous)
+{
+    cbloom_bitmap *map = bitmapFromFile(-1, 4096);
+    fail_unless(map != NULL);
+    fail_unless(bitmapBitsize(map) == 8*4096);
+}
+END_TEST
+
+START_TEST(bitmap_size_file)
+{
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/mmap_size_file", 8196, 1, 1);
+    fail_unless(map != NULL);
+    fail_unless(bitmapBitsize(map) == 8*8196);
+    unlink("/tmp/mmap_size_file");
+}
+END_TEST
+
+/*
+ * int bitmapFlush(cbloom_bitmap *map) {
+ */
+START_TEST(flush_bitmap_anonymous)
+{
+    cbloom_bitmap *map = bitmapFromFile(-1, 4096);
+    fail_unless(map != NULL);
+    fail_unless(bitmapFlush(map) == 0);
+}
+END_TEST
+
+START_TEST(flush_bitmap_file)
+{
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/mmap_flush_bitmap", 8196, 1, 1);
+    fail_unless(map != NULL);
+    fail_unless(bitmapFlush(map) == 0);
+    unlink("/tmp/mmap_flush_bitmap");
+}
+END_TEST
+
+START_TEST(flush_bitmap_null)
+{
+    fail_unless(bitmapFlush(NULL) == -EINVAL);
+}
+END_TEST
+
+/*
+ * int bitmapClose(cbloom_bitmap *map) {
+ */
+
+START_TEST(close_bitmap_anonymous)
+{
+    cbloom_bitmap *map = bitmapFromFile(-1, 4096);
+    fail_unless(map != NULL);
+    fail_unless(bitmapClose(map) == 0);
+    fail_unless(map->mmap == NULL);
+}
+END_TEST
+
+START_TEST(close_bitmap_file)
+{
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/mmap_close_bitmap", 8196, 1, 1);
+    fail_unless(map != NULL);
+    fail_unless(bitmapClose(map) == 0);
+    fail_unless(map->mmap == NULL);
+    unlink("/tmp/mmap_close_bitmap");
+}
+END_TEST
+
+START_TEST(close_bitmap_null)
+{
+    fail_unless(bitmapClose(NULL) == -EINVAL);
+}
+END_TEST
+
+/*
+ *#define BITMAP_GETBIT(map, idx)
+ */
+START_TEST(getbit_bitmap_anonymous_zero)
+{
+    cbloom_bitmap *map = bitmapFromFile(-1, 4096);
+    for (int idx = 0; idx < 4096*8 ; idx++) {
+        fail_unless(BITMAP_GETBIT(map, idx) == 0);
+    }
+}
+END_TEST
+
+START_TEST(getbit_bitmap_anonymous_one)
+{
+    cbloom_bitmap *map = bitmapFromFile(-1, 4096);
+    memset(map->mmap, 255, 4096);
+    for (int idx = 0; idx < 4096*8 ; idx++) {
+        fail_unless(BITMAP_GETBIT(map, idx) == 1);
+    }
+}
+END_TEST
+
+START_TEST(getbit_bitmap_file_zero)
+{
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/mmap_getbit_zero", 4096, 1, 1);
+    fail_unless(map != NULL);
+    for (int idx = 0; idx < 4096*8 ; idx++) {
+        fail_unless(BITMAP_GETBIT(map, idx) == 0);
+    }
+    unlink("/tmp/mmap_getbit_zero");
+}
+END_TEST
+
+START_TEST(getbit_bitmap_file_one)
+{
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/mmap_getbit_one", 4096, 1, 1);
+    fail_unless(map != NULL);
+    memset(map->mmap, 255, 4096);
+    for (int idx = 0; idx < 4096*8 ; idx++) {
+        fail_unless(BITMAP_GETBIT(map, idx) == 1);
+    }
+    unlink("/tmp/mmap_getbit_one");
+}
+END_TEST
+
+START_TEST(getbit_bitmap_anonymous_one_onebyte)
+{
+    cbloom_bitmap *map = bitmapFromFile(-1, 4096);
+    map->mmap[1] = 128;
+    fail_unless(BITMAP_GETBIT(map, 8) == 1);
+}
+END_TEST
+
+
+/*
+ *#define BITMAP_SETBIT(map, idx, val)
+ */
+START_TEST(setbit_bitmap_anonymous_one_byte)
+{
+    cbloom_bitmap *map = bitmapFromFile(-1, 4096);
+    BITMAP_SETBIT(map, 1, 1);
+    fail_unless(map->mmap[0] == 64);
+}
+END_TEST
+
+START_TEST(setbit_bitmap_anonymous_one_byte_aligned)
+{
+    cbloom_bitmap *map = bitmapFromFile(-1, 4096);
+    BITMAP_SETBIT(map, 8, 1);
+    fail_unless(map->mmap[1] == 128);
+}
+END_TEST
+
+
+START_TEST(setbit_bitmap_anonymous_one)
+{
+    cbloom_bitmap *map = bitmapFromFile(-1, 4096);
+    for (int idx = 0; idx < 4096*8 ; idx++) {
+        BITMAP_SETBIT(map, idx, 1);
+    }
+    for (int idx = 0; idx < 4096; idx++) {
+        fail_unless(map->mmap[idx] == 255);
+    }
+}
+END_TEST
+
+START_TEST(setbit_bitmap_file_one)
+{
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/mmap_setbit_one", 4096, 1, 1);
+    fail_unless(map != NULL);
+    for (int idx = 0; idx < 4096*8 ; idx++) {
+        BITMAP_SETBIT(map, idx, 1);
+    }
+    for (int idx = 0; idx < 4096; idx++) {
+        fail_unless(map->mmap[idx] == 255);
+    }
+    unlink("/tmp/mmap_setbit_one");
+}
+END_TEST
+
+/**
+ * Test that flush does indeed write to disk
+ */
+START_TEST(flush_does_write) {
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/mmap_flush_write", 4096, 1, 1);
+    fchmod(map->fileno, 0777);
+    fail_unless(map != NULL);
+    for (int idx = 0; idx < 4096*8 ; idx++) {
+        BITMAP_SETBIT(map, idx, 1);
+    }
+    bitmapFlush(map);
+
+    cbloom_bitmap *map2 = bitmapFromFilename("/tmp/mmap_flush_write", 4096, 0, 0);
+    fail_unless(map2 != NULL);
+    for (int idx = 0; idx < 4096; idx++) {
+        fail_unless(map->mmap[idx] == 255);
+    }
+    unlink("/tmp/mmap_flush_write");
+}
+END_TEST
+
+START_TEST(close_does_flush) {
+    cbloom_bitmap *map = bitmapFromFilename("/tmp/mmap_close_flush", 4096, 1, 1);
+    fchmod(map->fileno, 0777);
+    fail_unless(map != NULL);
+    for (int idx = 0; idx < 4096*8 ; idx++) {
+        BITMAP_SETBIT(map, idx, 1);
+    }
+    bitmapClose(map);
+    free(map);
+
+    map = bitmapFromFilename("/tmp/mmap_close_flush", 4096, 0, 0);
+    fail_unless(map != NULL);
+    for (int idx = 0; idx < 4096; idx++) {
+        fail_unless(map->mmap[idx] == 255);
+    }
+    unlink("/tmp/mmap_close_flush");
+}
+END_TEST
+
