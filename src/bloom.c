9d0ffd50b7b9854a643d8f319e59aa314b5b2bb7
Armon Dadgar <armon.dadgar@gmail.com>
2012-01-31 16:44:15 -0800

    Rethinking the bloom API

--- src/bloom.c
+++ src/bloom.c
@@ -1,97 +1,15 @@
 #include "bloom.h"
 
-/*
- * Static definitions
- */
-/*
-static size_t bloomFilterExtraBuffer();
-static unsigned int bloomFilterReadKNum(cbloom_bitmap* map);
-static void bloomFilterWriteKNum(cbloom_bitmap* map, unsigned int k_num);
-static unsigned long long bloomFilterReadCount(cbloom_bitmap* map);
-static void bloomFilterWriteCount(cbloom_bitmap* map, unsigned long long count);
-*/
-
 /**
  * Creates a new bloom filter using a given bitmap and k-value.
  * @arg map A cbloom_bitmap pointer.
  * @arg k_num The number of hash functions to use.
+ * @arg filter The filter to setup
+ * @return 0 for success. Negative for error.
  */
-cbloom_bloomfilter *createBloomFilter(cbloom_bitmap *map, size_t k_num) {
-    // Check our arguments for sanity
-    if (map == NULL || k_num < 1) {
-        return NULL;
-    }
-
-    /*
-    // Get the bitmap size, check we have enough room
-    size_t bitmap_size = bitmapBitsize(map) - 8 * bloomFilterExtraBuffer();
-    if (map->size <= bloomFilterExtraBuffer()) {
-        return NULL;
-    }
-
-    // Create the bloom filter
-    cbloom_bloomfilter *filter = (cbloom_bloomfilter*)calloc(1, sizeof(cbloom_bloomfilter));
-    filter->map = map;
-    filter->bitmap_size = bitmap_size;
-
-    // Read the existing count
-    filter->count = bloomFilterReadCount(map);
-
-    // Read the existing k_num, or set it to k_num if 0
-    filter->k_num = bloomFilterReadKNum(map);
-    if (filter->k_num == 0) {
-        filter->k_num = k_num;
-    }
-
-    // Check for compatibility mode, and remove the compatibility bit.
-    if (filter->k_num & COMPATIBILITY_MODE) {
-        filter->compat_mode = 1;
-        filter->k_num = ~COMPATIBILITY_MODE & filter->k_num;
-    }
-
-    // Allocate buffer space for the hashes
-    filter->hashes = calloc(filter->k_num, sizeof(size_t));
-
-    // Set the offset size
-    filter->offset = bitmap_size / filter->k_num;
-
-    // Return the filter
-    return filter;
-    */
-    return NULL;
+int bf_from_bitmap(cbloom_bitmap *map, uint32_t k_num, cbloom_bloomfilter *filter) {
+    return 0;
 }
-
-/*
-// Returns the extra bytes we need to persist the filter
-static size_t bloomFilterExtraBuffer() {
-    // We have enough room to store the K num, and the item count.
-    return sizeof(int) + sizeof(unsigned long long);
-}
-
-// Reads the K Num from the bitmap
-static unsigned int bloomFilterReadKNum(cbloom_bitmap* map) {
-    unsigned int *slot = (unsigned int*)(map->mmap + (map->size / 8) + sizeof(unsigned long long));
-    return *slot;
-}
-
-// Write the K Num to the bitmap
-static void bloomFilterWriteKNum(cbloom_bitmap* map, unsigned int k_num) {
-    unsigned int *slot = (unsigned int*)(map->mmap + (map->size / 8) + sizeof(unsigned long long));
-    *slot = k_num;
-}
-
-// Reads the count from the bitmap
-static unsigned long long bloomFilterReadCount(cbloom_bitmap* map) {
-    unsigned long long *slot = (unsigned long long*)(map->mmap + (map->size / 8));
-    return *slot;
-}
-
-// Write the count to the bitmap
-static void bloomFilterWriteCount(cbloom_bitmap* map, unsigned long long count) {
-    unsigned long long *slot = (unsigned long long*)(map->mmap + (map->size / 8));
-    *slot = count;
-}
-*/
 
 /**
  * Adds a new key to the bloom filter.
@@ -99,7 +17,7 @@ static void bloomFilterWriteCount(cbloom_bitmap* map, unsigned long long count) 
  * @arg key The key to add
  * @returns 1 if the key was added, 0 if present. Negative on failure.
  */
-int bloomFilterAdd(cbloom_bloomfilter *filter, char* key) {
+int bf_add(cbloom_bloomfilter *filter, char* key) {
     return 0;
 }
 
@@ -109,14 +27,14 @@ int bloomFilterAdd(cbloom_bloomfilter *filter, char* key) {
  * @arg key The key to check 
  * @returns 1 if present, 0 if not present, negative on error.
  */
-int bloomFilterContains(cbloom_bloomfilter *filter, char* key) {
+int bf_contains(cbloom_bloomfilter *filter, char* key) {
     return 0;
 }
 
 /**
  * Returns the size of the bloom filter in item count
  */
-uint64_t bloomFilterSize(cbloom_bloomfilter *filter) {
+uint64_t bf_size(cbloom_bloomfilter *filter) {
     return 0;
 }
 
@@ -124,7 +42,7 @@ uint64_t bloomFilterSize(cbloom_bloomfilter *filter) {
  * Flushes the filter, and updates the metadata.
  * @return 0 on success, negative on failure.
  */
-int bloomFilterFlush(cbloom_bloomfilter *filter) {
+int bf_flush(cbloom_bloomfilter *filter) {
     return 0;
 }
 
@@ -132,8 +50,7 @@ int bloomFilterFlush(cbloom_bloomfilter *filter) {
  * Flushes and closes the filter. Does not close the underlying bitmap.
  * @return 0 on success, negative on failure.
  */
-int bloomFilterClose(cbloom_bloomfilter *filter) {
+int bf_close(cbloom_bloomfilter *filter) {
     return 0;
 }
-
 
--- src/bloom.h
+++ src/bloom.h
@@ -6,10 +6,9 @@
 #include <stdbool.h>
 
 /**
- * We use a magic header to see if the bloom filter is
- * new-style, or if it is a legacy filter.
+ * We use a magic header to identify the bloom filters.
  */
-const uint32_t MAGIC_V2_HEADER = 0xCB1005DD;  // Vaguely like CBLOOMDD
+const uint32_t MAGIC_HEADER = 0xCB1005DD;  // Vaguely like CBLOOMDD
 struct cbloom_filter_header {
     uint32_t magic;     // Magic 4 bytes
     uint32_t k_num;     // K_num value
@@ -18,30 +17,16 @@ struct cbloom_filter_header {
 } __attribute__ ((packed));
 typedef struct cbloom_filter_header cbloom_filter_header;
 
-struct cbloom_filter_legacy_header {
-    uint64_t count;     // Count of items
-    uint32_t k_num;     // K_num value
-} __attribute__ ((packed));
-typedef struct cbloom_filter_legacy_header cbloom_filter_legacy_header;
-
 /*
  * This is the struct we use to represent a bloom filter.
- * We use unions to be flexible to using legacy filters.
  */
 typedef struct {
-    cbloom_bitmap *map;     // Underlying bitmap
-    uint64_t offset;        // The offset size between hash regions
-    bool legacy;            // If true, we are in legacy mode.
-    union {          // Pointers to the headers
-        cbloom_filter_header *header;
-        cbloom_filter_legacy_header *legacy_header;
-    } header;
-    union {          // Buffer to store the hashes in
-        uint64_t *hashes;      
-        uint32_t *legacy_hash;
-    } hashes;
-    uint64_t bitmap_size;   // The size of the bitmap to use, minus buffers
-    unsigned char* mmap;    // Starting address of the bitmap region. Different based on header.
+    unsigned char* mmap;            // Starting address of the bitmap region.
+    cbloom_filter_header *header;   // Pointer to the header in the bitmap region
+    cbloom_bitmap *map;             // Underlying bitmap
+    uint64_t offset;                // The offset size between hash regions
+    uint64_t bitmap_size;           // The size of the bitmap to use, minus buffers
+    uint64_t *hashes;               // Pre-allocated buffers for the hashes 
 } cbloom_bloomfilter;
 
 
@@ -49,8 +34,10 @@ typedef struct {
  * Creates a new bloom filter using a given bitmap and k-value.
  * @arg map A cbloom_bitmap pointer.
  * @arg k_num The number of hash functions to use.
+ * @arg filter The filter to setup
+ * @return 0 for success. Negative for error.
  */
-cbloom_bloomfilter *createBloomFilter(cbloom_bitmap *map, size_t k_num);
+int bf_from_bitmap(cbloom_bitmap *map, uint32_t k_num, cbloom_bloomfilter *filter);
 
 /**
  * Adds a new key to the bloom filter.
@@ -58,7 +45,7 @@ cbloom_bloomfilter *createBloomFilter(cbloom_bitmap *map, size_t k_num);
  * @arg key The key to add
  * @returns 1 if the key was added, 0 if present. Negative on failure.
  */
-int bloomFilterAdd(cbloom_bloomfilter *filter, char* key);
+int bf_add(cbloom_bloomfilter *filter, char* key);
 
 /**
  * Checks the filter for a key
@@ -66,24 +53,24 @@ int bloomFilterAdd(cbloom_bloomfilter *filter, char* key);
  * @arg key The key to check 
  * @returns 1 if present, 0 if not present, negative on error.
  */
-int bloomFilterContains(cbloom_bloomfilter *filter, char* key);
+int bf_contains(cbloom_bloomfilter *filter, char* key);
 
 /**
  * Returns the size of the bloom filter in item count
  */
-uint64_t bloomFilterSize(cbloom_bloomfilter *filter);
+uint64_t bf_size(cbloom_bloomfilter *filter);
 
 /**
  * Flushes the filter, and updates the metadata.
  * @return 0 on success, negative on failure.
  */
-int bloomFilterFlush(cbloom_bloomfilter *filter);
+int bf_flush(cbloom_bloomfilter *filter);
 
 /**
  * Flushes and closes the filter. Does not close the underlying bitmap.
  * @return 0 on success, negative on failure.
  */
-int bloomFilterClose(cbloom_bloomfilter *filter);
+int bf_close(cbloom_bloomfilter *filter);
 
 #endif
 
