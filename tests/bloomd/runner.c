25def5fadc758e948e5a30d6d8b27da90b90cf08
Armon Dadgar <armon.dadgar@gmail.com>
2012-02-12 13:45:53 -0800

    Add a test to check the default configuration gets loaded.

--- tests/bloomd/runner.c
+++ tests/bloomd/runner.c
@@ -1,5 +1,6 @@
 #include <check.h>
 #include <stdio.h>
+#include "test_config.c"
 
 int main(void)
 {
@@ -10,7 +11,7 @@ int main(void)
 
     // Add the bitmap tests
     suite_add_tcase(s1, tc1);
-    //tcase_add_test(tc1, make_anonymous_bitmap);
+    tcase_add_test(tc1, test_config_get_default);
 
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
--- tests/bloomd/test_config.c
+++ tests/bloomd/test_config.c
@@ -1,4 +1,3 @@
-/* let g:syntastic_c_include_dirs = [ 'src/bloomd/'] */
 #include <check.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -9,10 +8,22 @@
 #include <errno.h>
 #include "config.h"
 
-START_TEST(make_anonymous_bitmap)
+START_TEST(test_config_get_default)
 {
-    int res = 0;
+    bloom_config config;
+    int res = config_from_filename(NULL, &config);
     fail_unless(res == 0);
+    fail_unless(config.tcp_port == 8673);
+    fail_unless(config.udp_port == 8674);
+    fail_unless(strcmp(config.data_dir, "/tmp/bloomd") == 0);
+    fail_unless(strcmp(config.log_level, "DEBUG") == 0);
+    fail_unless(config.syslog_log_level == LOG_DEBUG);
+    fail_unless(config.initial_capacity == 100000);
+    fail_unless(config.default_probability == 1e-4);
+    fail_unless(config.scale_size == 4);
+    fail_unless(config.probability_reduction == 0.9);
+    fail_unless(config.flush_interval == 60);
+    fail_unless(config.cold_interval == 3600);
 }
 END_TEST
 
