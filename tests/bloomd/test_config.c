de2e0a1abce71ea6b17857f030c131de6c3148e6
Armon Dadgar <armon.dadgar@gmail.com>
2012-02-17 13:13:33 -0800

    Add a test for a bad filename

--- tests/bloomd/test_config.c
+++ tests/bloomd/test_config.c
@@ -27,4 +27,25 @@ START_TEST(test_config_get_default)
 }
 END_TEST
 
+START_TEST(test_config_bad_file)
+{
+    bloom_config config;
+    int res = config_from_filename("/tmp/does_not_exist", &config);
+    fail_unless(res == -ENOENT);
+
+    // Should get the defaults...
+    fail_unless(config.tcp_port == 8673);
+    fail_unless(config.udp_port == 8674);
+    fail_unless(strcmp(config.data_dir, "/tmp/bloomd") == 0);
+    fail_unless(strcmp(config.log_level, "DEBUG") == 0);
+    fail_unless(config.syslog_log_level == LOG_DEBUG);
+    fail_unless(config.initial_capacity == 100000);
+    fail_unless(config.default_probability == 1e-4);
+    fail_unless(config.scale_size == 4);
+    fail_unless(config.probability_reduction == 0.9);
+    fail_unless(config.flush_interval == 60);
+    fail_unless(config.cold_interval == 3600);
+}
+END_TEST
+
 
