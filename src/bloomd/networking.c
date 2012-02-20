6848f9faff2204449b95f95d9f4c9b51d8003c7d
Armon Dadgar <armon.dadgar@gmail.com>
2012-02-19 18:23:09 -0800

    Working on filling in the connection handlers.

--- src/bloomd/networking.c
+++ src/bloomd/networking.c
@@ -14,6 +14,7 @@
 #include <sys/socket.h>
 #include <pthread.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 /**
  * Default listen backlog size for
@@ -30,11 +31,19 @@
  */
 #define INIT_CONN_LIST_SIZE 1024
 
+/**
+ * How big should the default connection
+ * buffer size be. One page seems reasonable
+ * since most requests will not be this large
+ */
+#define INIT_CONN_BUF_SIZE 4096
+
 
 /**
  * Stores the thread specific user data.
  */
 typedef struct {
+    bloom_networking *netconf;
     ev_io *watcher;
     int ready_events;
 } worker_ev_userdata;
@@ -63,16 +72,20 @@ struct bloom_networking {
     int udp_listener_fd;
     ev_io tcp_client;
     ev_io udp_client;
+    ev_async loop_async;      // Allows async interrupts
     volatile int should_run;  // Should the workers continue to run
     volatile int num_threads; // Number of threads in the threads list
     pthread_t *threads;       // Array of thread references
     int conn_list_size;       // Maximum size of conns list
     conn_info **conns;        // An array of pointers to conn_info objects
+    pthread_mutex_t conns_lock; // Protects conns and conn_list_size
 };
 
 
 // Static typedefs
 static void prepare_event(ev_io *watcher, int revents);
+static void handle_async_event(ev_async *watcher, int revents);
+static void handle_new_client(int listen_fd, worker_ev_userdata* data);
 static void invoke_event_handler(worker_ev_userdata* data);
 
 
@@ -146,6 +159,7 @@ int init_networking(bloom_config *config, bloom_networking **netconf_out) {
 
     // Initialize
     pthread_mutex_init(&netconf->leader_lock, NULL);
+    pthread_mutex_init(&netconf->conns_lock, NULL);
     netconf->config = config;
     netconf->should_run = 1;
     netconf->num_threads = 0;
@@ -168,6 +182,10 @@ int init_networking(bloom_config *config, bloom_networking **netconf_out) {
         free(netconf);
         return 1;
     }
+
+    // Setup the async handler
+    ev_async_init(&netconf->loop_async, handle_async_event);
+    ev_async_start(&netconf->loop_async);
 
     // Success!
     *netconf_out = netconf;
@@ -196,12 +214,112 @@ static void prepare_event(ev_io *watcher, int revents) {
 
 
 /**
+ * Called when a message is sent to netconf->loop_async.
+ * This is usually to signal that some internal control
+ * flow related to the event loop needs to take place.
+ * For example, we might need to re-enable some ev_io* watchers,
+ * or exit the loop.
+ */
+static void handle_async_event(ev_async *watcher, int revents) {
+
+}
+
+
+/**
+ * Invoked when a TCP listening socket fd is ready
+ * to accept a new client. Accepts the client, initializes
+ * the connection buffers, and prepares to start listening
+ * for client data
+ */
+static void handle_new_client(int listen_fd, worker_ev_userdata* data) {
+    // Accept the client connection
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
+    int client_fd = accept(listen_fd,
+                        (struct sockaddr*)&client_addr,
+                        &client_addr_len);
+
+    // Debug info
+    syslog(LOG_DEBUG, "Accepted client connection: %s %d",
+            inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    /**
+     * Check if we have a cached conn_info object for this fd
+     * in the conns list already. If not, then we need to allocate
+     * a conn_info object and possibly resize the conns array.
+     */
+    conn_info *conn = NULL;
+    if (client_fd < data->netconf->conn_list_size) {
+        conn = data->netconf->conns[client_fd];
+    }
+
+    if (conn == NULL) {
+        conn = calloc(1, sizeof(conn_info));
+        conn->buf_size = INIT_CONN_BUF_SIZE;
+        conn->buffer = calloc(INIT_CONN_BUF_SIZE, sizeof(char));
+
+        // Lock the conns list
+        pthread_mutex_lock(&data->netconf->conns_lock);
+
+        // Resize if necessary
+        if (client_fd >= data->netconf->conn_list_size) {
+            // Keep doubling until we have enough space
+            int new_size = 2*data->netconf->conn_list_size;
+            while (new_size <= client_fd) {
+                new_size *= 2;
+            }
+
+            // Allocate a new list and copy the old entries
+            conn_info **new_conns = calloc(new_size, sizeof(conn_info*));
+            memcpy(new_conns,
+                   data->netconf->conns,
+                   sizeof(conn_info*)*data->netconf->conn_list_size);
+
+            // Flip to the new list
+            free(data->netconf->conns);
+            data->netconf->conns = new_conns;
+            data->netconf->conn_list_size = new_size;
+        }
+
+        // Store a reference to this connection buffer
+        data->netconf->conns[client_fd] = conn;
+
+        // Release
+        pthread_mutex_unlock(&data->netconf->conns_lock);
+    }
+
+    // Initialize the libev stuff
+    ev_io_init(&conn->client, prepare_event, client_fd, EV_READ);
+}
+
+/**
  * Reads the thread specific userdata to figure out what
  * we need to handle. Things that purely effect the network
  * stack should be handled here, but otherwise we should defer
  * to the connection handlers.
  */
 static void invoke_event_handler(worker_ev_userdata* data) {
+    // Get the offending handle
+    ev_io *watcher = data->watcher;
+    int fd = watcher->fd;
+
+    // Check if this is either of the listeners
+    if (fd == data->netconf->tcp_listener_fd) {
+        // Accept the new client
+        handle_new_client(fd, data);
+        return;
+
+    } else if (fd == data->netconf->udp_listener_fd) {
+
+        return;
+    }
+
+    /*
+     * If it is not a listener, it must be a connected
+     * client. We should just read all the available data,
+     * append it to the buffers, and then invoke the
+     * connection handlers.
+     */
 
 }
 
@@ -215,6 +333,9 @@ static void invoke_event_handler(worker_ev_userdata* data) {
 void start_networking_worker(bloom_networking *netconf) {
     // Allocate our user data
     worker_ev_userdata data;
+    data.netconf = netconf;
+    data.watcher = NULL;
+    data.ready_events = 0;
     int registered = 0;
 
     // Run forever until we are told to halt
