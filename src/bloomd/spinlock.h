c024f67a245d8c84b22c81e65e350c705ddee32d
Armon Dadgar <armon.dadgar@gmail.com>
2012-02-23 13:58:05 -0800

    Convenience header to help typedef spinlocks in a portabble way.

---
+++ src/bloomd/spinlock.h
@@ -0,0 +1,30 @@
+#ifndef BLOOM_SPINLOCK_H
+#define BLOOM_SPINLOCK_H
+
+#ifdef __MACH__
+// On OSX use OSAtomic
+#include <libkern/OSAtomic.h>
+typedef OSSpinLock bloom_spinlock;
+#define INIT_BLOOM_SPIN(spin) { *spin = OS_SPINLOCK_INIT; }
+#define LOCK_BLOOM_SPIN(spin) { OSSpinLockLock(spin); }
+#define UNLOCK_BLOOM_SPIN(spin) { OSSpinLockUnlock(spin); }
+
+#else
+#ifdef _POSIX_SPIN_LOCKS
+// On most POSIX systems, use pthreads spin locks
+typedef pthread_spinlock_t bloom_spinlock;
+#define INIT_BLOOM_SPIN(spin) { pthread_spin_init(spin, PTHREAD_PROCESS_PRIVATE); }
+#define LOCK_BLOOM_SPIN(spin) { pthread_spin_lock(spin); }
+#define UNLOCK_BLOOM_SPIN(spin) { pthread_spin_unlock(spin); }
+
+#else
+// Fall back onto a mutex
+typedef pthread_mutex_t bloom_spinlock;
+#define INIT_BLOOM_SPIN(spin) { pthread_mutex_init(spin, NULL); }
+#define LOCK_BLOOM_SPIN(spin) { pthread_mutex_lock(spin); }
+#define UNLOCK_BLOOM_SPIN(spin) { pthread_mutex_unlock(spin); }
+
+#endif
+#endif
+
+#endif
