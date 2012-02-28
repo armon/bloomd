e7388d794b145000821aa374567df97db2439d77
Armon Dadgar <armon.dadgar@gmail.com>
2012-02-28 15:41:35 -0800

    Adding new tests

--- tests/bloomd/test_filtmgr.c
+++ tests/bloomd/test_filtmgr.c
@@ -26,3 +26,67 @@ START_TEST(test_mgr_init_destroy)
 }
 END_TEST
 
+START_TEST(test_mgr_create_drop)
+{
+    bloom_config config;
+    int res = config_from_filename(NULL, &config);
+    fail_unless(res == 0);
+
+    bloom_filtmgr *mgr;
+    res = init_filter_manager(&config, &mgr);
+    fail_unless(res == 0);
+
+    res = filtmgr_create_filter(mgr, "bar1", NULL);
+    fail_unless(res == 0);
+
+    res = filtmgr_drop_filter(mgr, "bar1");
+    fail_unless(res == 0);
+
+    res = destroy_filter_manager(mgr);
+    fail_unless(res == 0);
+}
+END_TEST
+
+START_TEST(test_mgr_list)
+{
+    bloom_config config;
+    int res = config_from_filename(NULL, &config);
+    fail_unless(res == 0);
+
+    bloom_filtmgr *mgr;
+    res = init_filter_manager(&config, &mgr);
+    fail_unless(res == 0);
+
+    res = filtmgr_create_filter(mgr, "bar1", NULL);
+    fail_unless(res == 0);
+    res = filtmgr_create_filter(mgr, "bar2", NULL);
+    fail_unless(res == 0);
+
+    bloom_filter_list_head *head;
+    res = filtmgr_list_filters(mgr, &head);
+    fail_unless(res == 0);
+
+    int has_bar1 = 0;
+    int has_bar2 = 0;
+
+    bloom_filter_list *node = head->head;
+    while (node) {
+        if (strcmp(node->filter_name, "bar1") == 0)
+            has_bar1 = 1;
+        else if (strcmp(node->filter_name, "bar2") == 0)
+            has_bar2 = 1;
+        node = node->next;
+    }
+    fail_unless(has_bar1);
+    fail_unless(has_bar2);
+
+    res = filtmgr_drop_filter(mgr, "bar1");
+    fail_unless(res == 0);
+    res = filtmgr_drop_filter(mgr, "bar2");
+    fail_unless(res == 0);
+
+    res = destroy_filter_manager(mgr);
+    fail_unless(res == 0);
+}
+END_TEST
+
