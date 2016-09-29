54b135158bce5a93d1b135577fd4c6b830aabc77
FFng <ffng0x15@gmail.com>
2016-09-29 11:22:46 +0800

    add comments about `prob`

--- src/bloomd/config.c
+++ src/bloomd/config.c
@@ -273,7 +273,7 @@ int sane_default_probability(double prob) {
         syslog(LOG_ERR,
                "Probability cannot be equal-to or greater than 1!");
         return 1;
-    } else if (prob >= 0.10) {
+    } else if (prob >= 0.10) {  // prob must < 0.10
         syslog(LOG_ERR, "Default probability too high!");
         return 1;
     } else if (prob > 0.01) {
